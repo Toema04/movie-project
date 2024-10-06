00ca495127df1151faaa1fcbedffc004a0d25fdf
Youssef <140279042+aliveAndYoung@users.noreply.github.com>
2024-10-07 01:50:13 +0300

    setting things up

---
+++ src/components/initializing/Initialized.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Initialized = () => {
+  return (
+    <div className='flex justify-center text-slate-100 bg-green-950' >HELLO WORLD</div>
+  )
+}
+
+export default Initialized
